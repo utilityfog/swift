d91c3852c81b35fcc29a0aad57481a9c50606a60
Wonjae Oh <wonjae@live.unc.edu>
2024-07-26 20:55:45 +0900

    Practice1 update

--- practice1_app/practice1_app/ContentView.swift
+++ practice1_app/practice1_app/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Emoji: String, CaseIterable {
+        case 😎,😭,😇,😱
+    }
+    @State var selection: Emoji = .😎
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+//                For some reason, when using NavigationView in a macos app, one needs to have a default VStack before the actual VStack that will be displayed in order for NavigationView to properly contain the target VStack
+            }
+            .frame(width: 0)
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {
+                        emoji in Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .padding()
+            .navigationTitle("Emoji Lovers!")
         }
-        .padding()
     }
 }
 
